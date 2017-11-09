2d79348fea3c352ddeb4c57e0633e12f3c3d8964
Michael Vaganov <mvaganov@hotmail.com>
2017-11-09 08:14:25 -0800

    variable rename

--- Assets/Nonstandard Assets/Timer.cs
+++ Assets/Nonstandard Assets/Timer.cs
@@ -29,17 +29,17 @@ namespace NS {
 			return s_instance;
 		}
 		/// <summary>While this is zero, use system time. As soon as time becomes perturbed, by pause or time scale, keep track of game-time. To reset time back to realtime, use SynchToRealtime()</summary>
-		private long timer = 0;
+		private long alternativeTime = 0;
 		/// <summary>The timer counts in milliseconds, Unity measures in fractions of a second. This value reconciles fractional milliseconds.</summary>
 		private float leftOverTime = 0;
 
 		public static long NowRealtime() { return System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond; }
 
-		public long Now() { return timer == 0 ? NowRealtime() : timer; }
+		public long Now() { return alternativeTime == 0 ? NowRealtime() : alternativeTime; }
 
 		public static long now() { return Instance ().Now (); }
 
-		public void SynchToRealtime() { timer = 0; }
+		public void SynchToRealtime() { alternativeTime = 0; }
 
 		private int BestIndexFor(long soon){
 			int index = 0;
@@ -58,31 +58,31 @@ namespace NS {
 			Instance ().SetTimeout (action, delayMilliseconds);
 		}
 
-		void OnApplicationPause(bool paused) { if (timer == 0) { timer = Now (); } }
+		void OnApplicationPause(bool paused) { if (alternativeTime == 0) { alternativeTime = Now (); } }
 		
 		void Start () {
 			#if UNITY_EDITOR
 			// This method is run whenever the playmode state is changed.
 			UnityEditor.EditorApplication.pauseStateChanged += (UnityEditor.PauseState ps) => {
-				if (ps == UnityEditor.PauseState.Paused && timer == 0) { timer = Now(); }
+				if (ps == UnityEditor.PauseState.Paused && alternativeTime == 0) { alternativeTime = Now(); }
 			};
 			#endif
 			if (s_instance != null) { throw new System.Exception ("there should only be one timer!"); }
 			s_instance = this;
-			timer = 0;
+			alternativeTime = 0;
 		}
 
 		void Update () {
 			long now;
-			if (timer == 0) {
+			if (alternativeTime == 0) {
 				now = Now ();
-				if (Time.timeScale != 1) { timer = now; }
+				if (Time.timeScale != 1) { alternativeTime = now; }
 			} else {
 				float deltaTimeMs = (Time.deltaTime * 1000);
 				long deltaTimeMsLong = (long)(deltaTimeMs + leftOverTime);
-				timer += deltaTimeMsLong;
+				alternativeTime += deltaTimeMsLong;
 				leftOverTime = deltaTimeMs - deltaTimeMsLong;
-				now = timer;
+				now = alternativeTime;
 			}
 			while (queue.Count > 0 && queue [0].when <= now) {
 				ToDo todo = queue [0];
